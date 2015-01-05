305ff44dedab0c7c3fb8d4015b125b43bfd573dc
François Faubert <francois.faubert@iprospect.com>
2015-01-05 13:46:02 -0500

    added template loader to controller

--- src/Controller.php
+++ src/Controller.php
@@ -77,4 +77,17 @@ class Controller {
         die();
     }
 
+    /**
+     * @param string The name of the template to load (.php will be added to it)
+     * @param array an associative array of values to assign in the template
+     */
+    public static function loadTemplate($name, $values = array())
+    {
+        ob_start();
+        // expose local variables for the email template
+        extract($values);
+        include(get_template_directory() . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . $name . '.php');
+        return  ob_get_clean();
+    }
+
 }
