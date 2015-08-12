2dc88b231389c01b57705cbc6582d5c1b4304eab
Francois Faubert <francois.faubert@iprospect.com>
2015-08-12 10:36:23 -0400

    bugfix missing requirements

--- src/Model/Post.php
+++ src/Model/Post.php
@@ -9,22 +9,14 @@ use Strata\Model\CustomPostType\Entity;
  */
 class Post extends Entity {
 
-    public $routed = false;
     public $wpPrefix = "";
     public $belongs_to = array("Strata\Model\Taxonomy\Category");
-    public $configuration = array();
 
     /**
      * The permission level required for editing by the model
      * @var string
      */
     public $permissionLevel = 'edit_posts';
-
-    function __construct()
-    {
-        $this->admin_menus = Hash::normalize($this->admin_menus);
-        $this->belongs_to = Hash::normalize($this->belongs_to);
-    }
 
     /**
      * Returns a label object that exposes singular and plural labels
