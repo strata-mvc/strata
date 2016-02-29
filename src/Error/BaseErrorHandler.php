5b5fe046b18532f88acc62c84b3e24a76234c4bc
Francois Faubert <francois.faubert@iprospect.com>
2016-02-29 09:48:13 -0500

    do not use the error handler in cli mode

--- src/Error/BaseErrorHandler.php
+++ src/Error/BaseErrorHandler.php
@@ -41,7 +41,7 @@ class BaseErrorHandler
         $debugLevel = $this->getDebugLevel();
         $useDebugger = $this->useStrataDebugger();
 
-        if ($debugLevel > 0 && Strata::isDev()) {
+        if ($debugLevel > 0 && Strata::isDev() && !Strata::isCommandLineInterface()) {
             error_reporting(0); // we'll report it ourselves
             set_error_handler(array($this, 'handleError'), $debugLevel);
             set_exception_handler(array($this, 'wrapAndHandleException'));
