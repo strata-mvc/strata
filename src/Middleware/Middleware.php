314181b96ca9045e2e08f79e862f9c853c4a66ad
François Faubert <Francois Faubert>
2016-07-22 11:01:12 -0400

    added middleware generator

--- src/Middleware/Middleware.php
+++ src/Middleware/Middleware.php
@@ -7,6 +7,25 @@ namespace Strata\Middleware;
  */
 abstract class Middleware
 {
+    use StrataObjectTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getNamespaceStringInStrata()
+    {
+        return "Middleware";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getClassNameSuffix()
+    {
+        return "Initializer";
+    }
+
+
     /**
      * A list of shell commands being enabled by the middleware.
      * @var array
