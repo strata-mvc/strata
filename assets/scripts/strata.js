fceb2e097899424ca55340509a6c5d4d3bae506a
François Faubert <Francois Faubert>
2016-07-27 15:49:29 -0400

    refreshed the api

--- assets/scripts/strata.js
+++ assets/scripts/strata.js
@@ -1,12 +1,61 @@
 (function($, undefined) {
 
+    if ($('.home')) {
+        resizeHomeBoxes();
+        makeButtonPretty();
+    }
 
-    // var windowHeight = $(window).height(),
-    //     workingHeight = windowHeight < 400 ? 400 : windowHeight;
+    function resizeHomeBoxes()
+    {
+        var windowHeight = $(window).height(),
+            workingHeight = windowHeight < 400 ? 400 : windowHeight;
 
+        var boxes = $('.presentation-box');
+        boxes.css('height', workingHeight);
 
-    // $('.hero').css('height', workingHeight - 55);
-    // $('.presentation-box').css('height', workingHeight);
+        window.addEventListener("scroll", debounce(onScroll));
+        onScroll();
+    }
 
+    function makeButtonPretty()
+    {
+        $('.action-button').click(function(){
+             $('html, body').animate({
+                scrollTop: $(".presentation.presentating-right.bg-1").offset().top
+            });
+        });
+
+    }
+
+    function onScroll()
+    {
+        var top = (window.pageYOffset || document.documentElement.scrollTop)  - (document.documentElement.clientTop || 0),
+            height = parseInt($('.hero').height(), 10) - 200,
+            opacity = 1;
+
+        if (height < top) {
+            opacity = 0;
+        } else {
+            opacity = 1 - (top / height );
+        }
+
+        $('.action-button').css("opacity", opacity);
+    }
+
+    function debounce(func, wait, immediate)
+    {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
 
 })(jQuery);
