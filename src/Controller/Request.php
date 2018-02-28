e1bb40f93f91c22f7a77fd2b7569347909559577
Francois Faubert <frank@francoisfaubert.com>
2018-02-28 13:49:28 -0500

    Fixed object serialization

--- src/Controller/Request.php
+++ src/Controller/Request.php
@@ -308,7 +308,7 @@ class Request
     private function buildRequestData()
     {
         $strip_slashes_deep = function ($value) use (&$strip_slashes_deep) {
-            return is_array($value) ? array_map($strip_slashes_deep, $value) : sanitize_text_field(stripslashes($value));
+            return is_array($value) || is_object($value) ? array_map($strip_slashes_deep, $value) : sanitize_text_field(stripslashes($value));
         };
         $this->_GET = array_map($strip_slashes_deep, $_GET);
         $this->_POST = array_map($strip_slashes_deep, $_POST);
